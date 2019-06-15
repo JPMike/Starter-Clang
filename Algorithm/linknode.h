e2fef19c07952c297cbec372baf14f9913db036a
JPMike <jpmikemessage@gmail.com>
2019-06-15 18:11:13 +0800

    add linknode.h

---
+++ Algorithm/linknode.h
@@ -0,0 +1,12 @@
+//
+// Created by mac on 19-6-15.
+//
+
+#ifndef STARTER_CLANG_LINKNODE_H
+#define STARTER_CLANG_LINKNODE_H
+typedef struct node *link;
+struct node {
+    int item;
+    link next;
+};
+#endif //STARTER_CLANG_LINKNODE_H
